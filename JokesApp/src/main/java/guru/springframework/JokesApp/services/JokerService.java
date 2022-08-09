f6bc1aada92a08aca9eb027df102a2c31f4a4838
Vinícius <viniciusfloresribeiro@gmail.com>
2022-08-09 20:44:29 -0300

    create spring service

---
+++ JokesApp/src/main/java/guru/springframework/JokesApp/services/JokerService.java
@@ -0,0 +1,6 @@
+package guru.springframework.JokesApp.services;
+
+public interface JokerService {
+
+    String getJoke();
+}
---
+++ JokesApp/src/main/java/guru/springframework/JokesApp/services/JokerServiceImpl.java
@@ -0,0 +1,19 @@
+package guru.springframework.JokesApp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokerServiceImpl implements JokerService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokerServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
